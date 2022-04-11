a88282de40ffea6a1c9b0c56c509c9092d430b38
Hong Beomsun <ghsqjatns@naver.com>
2022-04-11 19:22:06 +0900

    refine en

--- refine_Data.js
+++ refine_Data.js
@@ -11,29 +11,9 @@ var JP_Translation_Data = {}
 
 var Videos = {}
 var Transcriptions = {}
-var Translations_EN = {}
+var Translations = {}
 
 
-
-/*
-class Video {
-    id: String, // 영상 ID
-    channel: String, // 영상 채널 ID
-    state: Number, // 0: 열림 (수정 가능), 1: 잠금
-
-    metadata: {
-        title: String, // 영상 제목 (수정이 가능한 필드)
-        description: String, // 영상 설명 (수정이 가능한 필드)
-        type: Number,  // 영상 타입 (풀 영상, 노래, 일반, 기타 등등)
-        url: String,// 영상 URL (수정 불가능)
-        duration: Number, // 영상 길이
-        uploadDate: Date, // 영상이 업로드 된 날짜
-        deleted: Boolean, // 영상이 삭제됐는 지에 대한 여부 (수정이 가능한 필드)
-    },
-
-    translations : [],
-}
-*/
 
 async function readFiles(){
     EN_Translation_Data = await fs.readFile('./JsonData/EN_Translation.json', 'utf8')
@@ -51,49 +31,78 @@ async function readFiles(){
 
 async function refineData_EN(){
     var nowdata = {}
-    console.log(EN_Translation_Data[0])
     for(var index = 0; index < EN_Translation_Data.length; index++){
         nowdata = EN_Translation_Data[index]
-
-        var _videoid = getVideoIDfromURL(nowdata['URL'])
+        var _videoid = getVideoIDfromURL(nowdata['URL'][0])
         if(Videos[_videoid])
             continue
+        
+        try{
+            console.log('GET videoid : ' + _videoid)
+            var _videoDetail = await getYoutubeVideoDetail(_videoid)
+            var _videoDetailSnippet = _videoDetail['items'][0]['snippet']
+            var _videoDetailContent = _videoDetail['items'][0]['contentDetails']
 
-        var _videoDetail = await getYoutubeVideoDetail(_videoid)
-        var _videoDetailSnippet = _videoDetail['items'][0]['snippet']
-        var _videoDetailContent = _videoDetail['items'][0]['contentDetails']
+            Videos[_videoid] = {
+                id: _videoid,
+                channel: _videoDetailSnippet['channelId'],
+                state: 0,
+                metadata: {
+                    title: _videoDetailSnippet['title'],
+                    description: _videoDetailSnippet['description'],
+                    type : nowdata['영상 분류'][0],
+                    url : 'https://youtu.be/' + _videoid,
+                    duration : getYoutubeVideoDuration(_videoDetailContent['duration']),
+                    uploadDate :_videoDetailSnippet['publishedAt'],
+                    deleted : false,
+                },
+                translations : ['en',]
+            }
 
-        Videos[_videoid] = {
-            id: _videoid,
-            channel: _videoDetailSnippet['channelId'],
-            state: 0,
-            metadata: {
-                title: _videoDetailSnippet['title'],
-                description: _videoDetailSnippet['description'],
-                type : -1,       //fix require
-                url : 'https://youtu.be/' + _videoid,
-                duration : getYoutubeVideoDuration(_videoDetailContent['duration']),
-                uploadDate :_videoDetailSnippet['publishedAt'],
-                deleted : false,
-            },
-            translations : [en,]
-        }
+            if(nowdata['한국어 자막파일'] != 'Null'){
+                transcriptions_func(_videoid, nowdata)
+            }
 
-        if(EN_Translation_Data['한국어 자막파일'] != 'Null'){
-            Transcriptions[_videoid] = {
-                videoId : _videoid,
-                state : -1,
-                caption_state : -1,
-                caption_uploads : -1,
-                caption_files : EN_Translation_Data['한국어 자막파일'][0],
-                contributors : [{}],
-                histories : [],
+            if(nowdata['영어 자막 파일'] != 'Null'){
+                translations_func(_videoid, nowdata, 'en')
             }
         }
-
-        if(EN_Translation_Data['']){
-
+        catch(e){
+            console.log(e)
         }
+    }
+}
+
+function transcriptions_func(_videoid, nowdata){
+    Transcriptions[_videoid] = {
+        videoId : _videoid,
+        state : nowdata['진행 상황 (from 받아쓰기 + 자막 싱크)'][0],
+        caption : {
+            caption_state : nowdata['진행 상황'],
+            caption_uploads : nowdata['진행 상황'],
+            caption_files : nowdata['한국어 자막파일'],
+        },
+        contributors : [],
+        histories : [],
+    }
+}
+
+function translations_func(_videoid, nowdata, lang){
+    Translations[lang] = {
+        videoId : _videoid,
+        state : nowdata['진행 상황'],
+        metadata : {
+            title : nowdata['제목 (영어 번역)'],
+            description : nowdata['세부 정보 (영어 번역)'],
+            state : nowdata['진행 상황'],
+            url : 'https://youtu.be/' + _videoid,
+        },
+        caption : {
+            state : nowdata['진행 상황'],
+            files : nowdata['영어 자막 파일'],
+        },
+        contributors : [],
+        histories : [],
     }
 }
 
@@ -124,10 +133,14 @@ function getYoutubeVideoDuration(str){
 
 async function main(){
     await readFiles()
-    refineData_EN()
+    await refineData_EN()
+
+    await fs.writeFile('./RefinedData/Videos.json', JSON.stringify(Videos, null, '\t'))
+    await fs.writeFile('./RefinedData/Transcriptions.json', JSON.stringify(Transcriptions, null, '\t'))
+    await fs.writeFile('./RefinedData/Translations.json', JSON.stringify(Translations, null, '\t'))
 }
-//main()
-test()
+main()
+//test()
 
 async function test(){
     var _videoid = 'KdWPnX7HVCY'
